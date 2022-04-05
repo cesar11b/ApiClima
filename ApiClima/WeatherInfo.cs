3eab9f3b1d33fe0f38b5d70eb86192e2780f6d1d
cesar11b <katanavivas@gmail.com>
2022-04-04 19:46:49 -0600

    escritura de algunas clases

---
+++ ApiClima/WeatherInfo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiClima
+{
+    class WeatherInfo
+    {
+
+        public class Cord
+        {
+
+            double lon { get; set; }
+            double lat { get; set; }
+
+        }
+
+
+
+        public class Weather
+        {
+
+            string Main { get; set; }
+            string Description { get; set; }
+            string Icon { get; set; }
+        }
+
+        public class Main
+        {
+
+            double Temp { get; set; }
+            double Pressure { get; set; }
+            double Humidity { get; set; }
+
+          
+        }
+
+        public class Wind
+        {
+            double Speed { get; set; }
+
+
+        }
+
+        public class Sys
+        {
+
+            long Sunrise { get; set; }
+            long Sunset { get; set; }
+
+        }
+
+
+        public class Root
+        {
+
+
+
+        }
+
+
+
+    }
+}
